04d8038e8d23b32efdf808f9fe5877248880979e
jyh <j328801898@gmail.com>
2017-07-22 14:35:47 +0800

    initial

---
+++ CSharpCommonDll/CSharpCommonDll/Common.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharpCommonDll
+{
+    public partial class Jyh
+    {
+        //确定对话框
+        public bool ConfirmForm(string content, string title = "确认?")
+        {
+            MessageBoxButtons messButton = MessageBoxButtons.OKCancel;
+            DialogResult dr = MessageBox.Show(content, title, messButton);
+            if (dr == DialogResult.Cancel)
+                return false;
+            else
+                return true;
+        }
+    }
+}
---
+++ CSharpCommonDll/CSharpCommonDll/System.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCommonDll
+{
+    public partial class Jyh
+    {
+        //取第一块硬盘编号   
+        public String GetHardDiskId()
+        {
+            try
+            {
+                ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM Win32_PhysicalMedia");
+                String strHardDiskId = null;
+                foreach (var o in searcher.Get())
+                {
+                    var mo = (ManagementObject) o;
+                    strHardDiskId = mo["SerialNumber"].ToString().Trim();
+                    break;
+                }
+                return strHardDiskId;
+            }
+            catch
+            {
+                return "";
+            }
+        }
+    }
+}
