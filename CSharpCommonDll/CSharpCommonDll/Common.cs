28f7410ce35265b2ebfdb88a6a6f5e1483e4b1aa
jyh <j328801898@gmail.com>
2017-09-04 09:32:12 +0800

    Add single model

--- CSharpCommonDll/CSharpCommonDll/Common.cs
+++ CSharpCommonDll/CSharpCommonDll/Common.cs
@@ -1,23 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 
 namespace CSharpCommonDll
 {
     public partial class Jyh
     {
+        private readonly Jyh _instance = new Jyh();
+
+        private Jyh()
+        {
+        }
+
+        public Jyh GetInstance()
+        {
+            return _instance;
+        }
+
         //确定对话框
         public bool ConfirmForm(string content, string title = "确认?")
         {
-            MessageBoxButtons messButton = MessageBoxButtons.OKCancel;
-            DialogResult dr = MessageBox.Show(content, title, messButton);
+            var messButton = MessageBoxButtons.OKCancel;
+            var dr = MessageBox.Show(content, title, messButton);
             if (dr == DialogResult.Cancel)
                 return false;
-            else
-                return true;
+            return true;
         }
     }
 }
